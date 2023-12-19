58620d7752f5646f956ea6b313a185680ccf1a27
Fullhouse777KK <gafur777_97@mail.ru>
2023-12-19 16:26:02 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+// let x = +prompt('Введите число')
+// let y = +prompt('Введите степень')
+// answer = x
+// for (let i = 1; i = y; i++) {
+//       answer = answer * y;
+//     }
+//  alert(answer) 
+
+// do {
+//   let a = +prompt('Введите число')
+//   let b = +prompt('Введите степень')
+// } while (isNaN(a) || isNaN(b));
+// let answer = a ** b
+// alert(answer)
+
+var a = +prompt('Введите число ступенек')
+var b = prompt('Введите первый символ')
+var c = prompt('Введите второй символ')
+for (let i = 0; i < a; i++) {
+    let line = b
+for (let b = 0; b < i; b++) 
+      line += c
+      console.log(line)
+  }
+ 
